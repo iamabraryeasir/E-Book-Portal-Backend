472b2ac313d25bc82aa8eab43e3aad1d9a6233a6
Abrar Yeasir <iamabraryeasir@gmail.com>
2025-02-09 12:59:06 +0600

    refactor user registration logic to include error handling and improve code structure

--- src/user/user.controller.js
+++ src/user/user.controller.js
@@ -5,38 +5,48 @@ import { ApiResponse } from "../utils/ApiResponse.js";
 import { config } from "../config/config.js";
 
 const registerUser = async (req, res, next) => {
-  // getting the data
-  const { name, email, password } = req.body;
+  try {
+    // getting the data
+    const { name, email, password } = req.body;
 
-  // validate
-  if (!name?.trim() || !email?.trim() || !password?.trim()) {
-    return next(ApiResponse.error(400, "All fields are required."));
+    // validate
+    if (!name?.trim() || !email?.trim() || !password?.trim()) {
+      return next(ApiResponse.error(400, "All fields are required."));
+    }
+
+    // checking if the user already exists
+    const user = await User.findOne({ email });
+
+    if (user) {
+      return next(
+        ApiResponse.error(400, "User already exists with this email.")
+      );
+    }
+
+    // hash the password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // creating new user
+    const newUser = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    // token generation
+    const token = jwt.sign({ sub: newUser._id }, config.jwtSecret, {
+      expiresIn: "7d",
+    });
+
+    // response
+    res.status(200).json(
+      new ApiResponse(200, "Successfully registered user.", {
+        accessToken: token,
+      })
+    );
+  } catch (err) {
+    return next(ApiResponse.error(500, err.message));
   }
-
-  // checking if the user already exists
-  const user = await User.findOne({ email });
-
-  if (user) {
-    return next(ApiResponse.error(400, "User already exists with this email."));
-  }
-
-  // hash the password
-  const hashedPassword = await bcrypt.hash(password, 10);
-
-  // creating new user
-  const newUser = await User.create({ name, email, password: hashedPassword });
-
-  // token generation
-  const token = jwt.sign({ sub: newUser._id }, config.jwtSecret, {
-    expiresIn: "7d",
-  });
-
-  // response
-  res.status(200).json(
-    new ApiResponse(200, "Successfully registered user.", {
-      accessToken: token,
-    })
-  );
 };
 
 export { registerUser };
