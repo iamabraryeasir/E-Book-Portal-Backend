469a8043131003b2260ebc1c1b12d9c40b92c52b
Abrar Yeasir <iamabraryeasir@gmail.com>
2025-02-09 10:39:13 +0600

    add user registration route and controller

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { globalErrorHandler } from "./middlewares/globalErrorHandler.js";
+import { userRouter } from "./user/user.router.js";
 
 const app = express();
 
@@ -7,6 +8,8 @@ const app = express();
 app.get("/", (req, res) => {
   res.json({ message: "Hello, world!" });
 });
+
+app.use("/api/users", userRouter);
 
 // global error handler
 app.use(globalErrorHandler);
---
+++ src/user/user.controller.js
@@ -0,0 +1,8 @@
+const registerUser = async (req, res, next) => {
+  // getting the data from frontend
+  const { name, email, password } = req.body;
+
+  
+};
+
+export { registerUser };
